9ceccccdaeb6c1b6909efc38eab566fa66b3ab19
Nana Emmanuel <naelcodes.github@gmail.com>
2024-10-12 03:15:32 +0000

    chore: setup devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "\n >>>>>> SETUP START \n"
+
+git config --global --add safe.directory /workspaces/dxcli
+echo "\n >> Configured git \n"
+
+ . /home/vscode/.bashrc
+ echo "\n >> Appended pnpm global-bin-dir setting lines to /home/vscode/.bashrc"
+
+
+if [ ! -d "node_modules" ] && [ -f "package.json" ]; then
+  echo "\n >> node_modules folder not found and package.json exists. Running pnpm install...\n"
+  pnpm install --frozen-lockfile
+elif [ ! -f "package.json" ]; then
+  echo "\n >> package.json not found. Cannot run pnpm install."
+else
+  echo "\n >> node_modules folder already exists. Skipping pnpm install."
+fi
+
+
+pnpm link --global
+echo "\n >> linked  cli globally\n"
+
+echo "\n >>>>> SETUP DONE"
