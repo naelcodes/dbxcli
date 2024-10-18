ff07f60e051a74943f0e2a82c43b26f7a6534769
Nana Emmanuel <naelcodes.github@gmail.com>
2024-10-18 20:51:30 +0000

    feat(helpers): add function that handles directus cli prompts

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,19 +1,26 @@
 import type {Ora} from 'ora';
 import type {LOG_LEVEL} from './constants.js';
 
-
 export type LOG_LEVEL_TYPE = (typeof LOG_LEVEL)[keyof typeof LOG_LEVEL];
 
 export type logAndAbortOptions = {
-    spinner?:Ora;
-	context?:string;
-	loglevel:LOG_LEVEL_TYPE;	
-}
+	spinner?: Ora;
+	context?: string;
+	loglevel: LOG_LEVEL_TYPE;
+};
 
 export type Result<T, E extends Error = Error> = [T | null, E | null];
 export type AsyncResult<T, E extends Error = Error> = Promise<Result<T, E>>;
 
 export type CommandInput = {
-    name: string,
-    value: boolean | string;
+	name: string;
+	value: boolean | string;
+};
+
+export type DirectusCliPromptsAnswers = {
+	extensionType?: string;
+	extensionName?: string;
+	language?: string;
+	path?: string;
+	install?: 'y' | 'n';
 };
