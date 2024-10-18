f8b99347251d65c905f74490dff1e67158a4594b
Nana Emmanuel <naelcodes.github@gmail.com>
2024-10-18 18:40:09 +0000

    chore(cli): fix typo in cli description

--- src/cli/program.ts
+++ src/cli/program.ts
@@ -7,7 +7,7 @@ export const program = new Command();
 program
 	.name('dbxcli')
 	.usage('[commands] [arguments] [options]')
-	.description('CLI tool for scaffolding Directus bundle extensions with utilities to enhance developer experie',
+	.description('CLI tool for scaffolding Directus bundle extensions with utilities to enhance developer experience',
 	)
     .configureHelp({
         subcommandTerm: (cmd) => `${chalk.green(cmd.name())} ${cmd.usage()}`,
