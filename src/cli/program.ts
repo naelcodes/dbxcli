08c6c8ad1362368e85b7cde428064a607edbee5c
Nana Emmanuel <naelcodes.github@gmail.com>
2024-10-20 01:38:57 +0000

    feat(cli): update the style of cli UI
    update the style of help command output, error output and help message shown after an error occured

--- src/cli/program.ts
+++ src/cli/program.ts
@@ -1,6 +1,7 @@
 import {Command} from 'commander';
 import chalk from 'chalk';
 import {getCliVersion} from '@lib/helpers';
+import {LOG_PREFIX} from '@lib/ui';
 
 export const program = new Command();
 
@@ -10,5 +11,15 @@ program
 	.description('CLI tool for scaffolding Directus bundle extensions with utilities to enhance developer experience')
 	.configureHelp({
 		subcommandTerm: (cmd) => `${chalk.green(cmd.name())} ${cmd.usage()}`,
+		optionTerm: (option) => `${chalk.green(option.flags)}`,
+		commandUsage: (cmd) => {
+			const cmdName = cmd.alias() ? `${cmd.name()}|${cmd.alias()}` : cmd.name();
+			return `${chalk.green(`${cmdName} ${cmd.usage()}`)}`
+		},
+		argumentTerm:(argument) => `${chalk.green(argument.name())}`		
 	})
+	.configureOutput({
+		outputError: (str, write) => write(`${LOG_PREFIX.ERROR} ${str.replace('error: ','')}\n`)
+	})
+	.showHelpAfterError(`${LOG_PREFIX.INFO} Use ${chalk.green.bold('dbxcli [command] --help')} for more information on command usage.`)
 	.version(await getCliVersion());
